8d8bcab6b8b5793219a77cb4481e4f061c80152b
Walter Kimaro <wakyj07@gmail.com>
2023-10-19 15:08:19 +0300

    Fix drag and drop bug

--- app/components/DragDropList.jsx
+++ app/components/DragDropList.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { ClientOnly } from "remix-utils";
+import { objectFieldChoices } from "~/utils";
 
 const reorder = (list, startIndex, endIndex) => {
 	const result = Array.from(list);
@@ -13,9 +16,26 @@ const getItemStyle = (isDragging, draggableStyle) => ({
 	...draggableStyle,
 });
 
+const StrictModeDroppable = ({ children, ...props }) => {
+	const [enabled, setEnabled] = useState(false);
+
+	useEffect(() => {
+		const animation = requestAnimationFrame(() => setEnabled(true));
+
+		return () => {
+			cancelAnimationFrame(animation);
+			setEnabled(false);
+		};
+	}, []);
+
+	if (!enabled) return null;
+
+	return <Droppable {...props}>{children}</Droppable>;
+};
+
 const DragDropList = ({
 	items,
-	idKey = "_id",
+	idKey = "value",
 	onChange,
 	children,
 	className,
@@ -27,44 +47,48 @@ const DragDropList = ({
 	};
 
 	return (
-		<DragDropContext onDragEnd={onDragEnd}>
-			<Droppable droppableId="droppable">
-				{(provided) => (
-					<div
-						className={className}
-						{...provided.droppableProps}
-						ref={provided.innerRef}
-					>
-						{items.map((item, index) => {
-							console.log("Id: ", item[idKey]);
-
-							return (
-								<Draggable
-									key={item[idKey]}
-									draggableId={item[idKey]}
-									index={index}
-								>
-									{(provided, snapshot) => (
-										<div
-											ref={provided.innerRef}
-											{...provided.draggableProps}
-											{...provided.dragHandleProps}
-											style={getItemStyle(
-												snapshot.isDragging,
-												provided.draggableProps.style
-											)}
+		<ClientOnly>
+			{() => (
+				<DragDropContext onDragEnd={onDragEnd}>
+					<StrictModeDroppable droppableId="droppable">
+						{(provided) => (
+							<div
+								className={className}
+								{...provided.droppableProps}
+								ref={provided.innerRef}
+							>
+								{objectFieldChoices(items).map(
+									(item, index) => (
+										<Draggable
+											key={item[idKey]}
+											draggableId={item[idKey]}
+											index={index}
 										>
-											{children({ item })}
-										</div>
-									)}
-								</Draggable>
-							);
-						})}
-						{provided.placeholder}
-					</div>
-				)}
-			</Droppable>
-		</DragDropContext>
+											{(provided, snapshot) => (
+												<div
+													ref={provided.innerRef}
+													{...provided.draggableProps}
+													{...provided.dragHandleProps}
+													style={getItemStyle(
+														snapshot.isDragging,
+														provided.draggableProps
+															.style
+													)}
+												>
+													{children({ item })}
+												</div>
+											)}
+										</Draggable>
+									)
+								)}
+
+								{provided.placeholder}
+							</div>
+						)}
+					</StrictModeDroppable>
+				</DragDropContext>
+			)}
+		</ClientOnly>
 	);
 };
 
